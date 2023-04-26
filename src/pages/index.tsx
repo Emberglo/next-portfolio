80405ad2fc86b8cfa91f4378512afb5c3b4d072b
Ryan Carpenter <rccarpenter@outlook.com>
2023-04-26 13:15:49 -0600

    fix apostrophe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className='home-height w-full my-auto flex flex-col items-center justify-center'>
       <Image src={Me} alt='image of Ryan' height='200' width='200' className='rounded-full'></Image>
-      <h1 className='my-2 text-3xl'>Hi! I'm Ryan.</h1>
+      <h1 className='my-2 text-3xl'>Hi! I&#39;m Ryan.</h1>
       <h2 className='my-2 text-xl'>Software Developer</h2>
       <p className='my-2 w-3/4'>I am a software developer based in Boise, Idaho. I have experience working on the front and back end of websites and applications in multiple tech stacks. My favorite work to do is build beautiful and functional front ends that make big impacts on businesses and users. This site is built with NextJS and TailwindCSS, and I have experience in Angular, PHP, C#, and others.</p>
       <p className='my-2 text-xl'>Contact me on socials:</p>
